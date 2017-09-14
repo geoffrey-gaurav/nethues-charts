9f311a9d2a6a91b510e20f4ef6e021651a7f28ab
sherman <sherman@123789.org>
2017-09-14 12:20:46 +0530

    first commit

---
+++ src/sample.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample-component',
+  template: `<h1>Sample component1</h1>`
+})
+export class SampleComponent {
+
+  constructor() {
+  	/*https://github.com/jvandemo/generator-angular2-library*/
+  }
+
+}
